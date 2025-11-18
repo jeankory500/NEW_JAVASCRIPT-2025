37e47c670846363aa48a276002de96ca54398088
JeankoryV <124745650+jeankory500@users.noreply.github.com>
2025-11-17 21:07:38 -0500

    Javascript added

---
+++ javascript.js
@@ -0,0 +1,35 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName);
+console.log(lastName);
+////////////////////////////////////////////
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+////////////////////////////////////////////
+const pi = 3.14;
+
+
+
+console.log(pi); // What will be output?
+
+//////////////////////////////////////////
+console.log(23 + 97);
+
+console.log(20 + 5 + 111 + 30 + 44 + 10);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+let a = 25;
+console.log(a);
+
+
+
